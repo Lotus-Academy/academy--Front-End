6b2db01753d37c07815326e80c7b6b6b327807f2
GABA Kossi Martinien <martiniengaba123@gmail.com>
2026-04-17 06:01:04 +0100

    adding cloudflare for the video streaming

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://kossimartiniengaba.tech',
+    apiUrl: 'https://api-spring.kossimartiniengaba.tech',
     //apiUrl: 'http://localhost:8080',
     clientApiUrl: 'https://lotus-academy.github.io/academy--Front-End'
 
